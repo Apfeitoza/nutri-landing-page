971798174447a4b316686081e00c490178388a84
Apfeitoza <annapaulaasf@gmail.com>
2026-03-10 17:39:43 -0300

    feat(sobre): implementar seção sobre mim e credenciais

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
+//link ativo-inativo
+const navLinks = document.querySelectorAll('.nav-link'); 
+navLinks.forEach(link => {
+  link.addEventListener('click', function() {    
+    navLinks.forEach(l => l.classList.remove('active'));       
+    this.classList.add('active');
+  });
+});
+
+//Swiper da hero
 document.addEventListener("DOMContentLoaded", () => {
   // Carrossel do Hero - Home
   const heroSliderElement = document.querySelector(".hero__slider");
 
   if (heroSliderElement) {
      new Swiper(".hero__slider", {
-      loop: false,
-      speed: 600,
+      loop: false,     
       effect: "fade",
       fadeEffect: {
         crossFade: true,
